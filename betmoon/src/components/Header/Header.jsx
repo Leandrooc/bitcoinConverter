440113b1207e819f3673dc3e69ebf78b0cdc657e
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-04-24 20:30:04 -0300

    fix: Header with image and Bitcoin converter text

--- betmoon/src/components/Header/Header.jsx
+++ betmoon/src/components/Header/Header.jsx
@@ -5,9 +5,10 @@ export function Header() {
   return (
     <header>
       <ul>
-        <li>Home</li>
-        <li>Search</li>
-        <li>Login</li>
+        <li>
+          <img width="60px" src="https://www.pngitem.com/pimgs/m/108-1082679_bitcoin-png-transparent-background-bitcoin-png-png-download.png" alt="bitcoin logo" />
+        </li>
+        <li>Bitcoin converter</li>
       </ul>
     </header>
   )
