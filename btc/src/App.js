cda7aa9a2f550638e90ece871a7c077b3a251b12
Leandro de Oliveira <19leandro3quietmsl@gmail.com.br>
2022-04-24 21:38:47 -0300

    fix: Folder name

---
+++ btc/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Currency } from "./components/Currency/Currency";
+import { Header } from "./components/Header/Header";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Currency />
+    </>
+  );
+}
+
+export default App;
---
+++ btc/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Header.css'
+
+export function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <img width="59px" 
+            src="https://www.pngitem.com/pimgs/m/108-1082679_bitcoin-png-transparent-background-bitcoin-png-png-download.png"
+            alt="bitcoin logo"
+          />
+        </li>
+        <li>Bitcoin converter</li>
+      </ul>
+    </header>
+  )
+};
